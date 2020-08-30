03c2f6d03139cd847e1e90c6b1ecd159505f559f
Wildiney Di Masi <wildiney@gmail.com>
2020-08-30 19:31:19 -0300

    fix: spellchek

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer () {
   return (
     <footer>
-      <p>&copy; 2020 - slicedpixel.com</p>
+      <p>&copy; 2020 - <a href="https://slicedpixel.com">slicedpixel.com</a></p>
     </footer>
   )
 }
