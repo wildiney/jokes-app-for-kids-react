8ac045a72eceefa1282fb884a17b15d5d5dbcecc
Wildiney Di Masi <wildiney@gmail.com>
2020-08-30 01:02:33 -0300

    feat: header and footer added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react'
+
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 import Jokes from './components/Jokes/Jokes'
 
 import './App.css'
@@ -6,7 +9,9 @@ import './App.css'
 function App () {
   return (
     <div className="App">
+      <Header />
       <Jokes />
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer () {
+  return (
+    <footer>
+      <p>&copy; 2020 - slicedpixel.com</p>
+    </footer>
+  )
+}
+
+export default Footer
